07a1dfd7814645a54eefc80b1f3837370f0b4f31
Emehinola Idowu <hydeenoble39@gmail.com>
2016-10-18 21:55:46 +0100

    testing

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-require_once ('mailsender.php');
-?>
+//require_once ('mailsender.php');
+//?>
+<!---->
+<?php //echo (htmlspecialchars($_SERVER['PHP_SELF']))?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -13,7 +15,7 @@ require_once ('mailsender.php');
     <div class="container-fluid text-center main">
         <h1 class="header"><span class="glyphicon glyphicon-envelope"></span> WELCOME TO THE MAILING APP</h1>
 
-        <form action="<?php echo (htmlspecialchars($_SERVER['PHP_SELF']))?>" class="form text-center" method="post" enctype="multipart/form-data">
+        <form action="mailsender.php" class="form text-center" method="post" enctype="multipart/form-data">
            <div class="row">
                <div class="col-sm-4 form-group">
                      <input type="text" class="form-control" name="sender_name" placeholder="From" />
